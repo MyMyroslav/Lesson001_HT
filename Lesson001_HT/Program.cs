c462c8a2a2e62a02729bc22c0fb286f3be470ed5
Myroslav <havrushkomyroslav@gmail.com>
2020-11-03 00:15:49 +0200

    Done-Lesson001_HT

---
+++ Lesson001_HT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson001_HT
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            double eyl = +2.7182818284590452;
+            Console.WriteLine($"Pi={pi},Eyl=+{eyl}");
+            
+        }
+    }
+}
---
+++ Lesson001_HT_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson001_HT_2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string first = "\nmy string1";
+            string second = "\tmy string2";
+            string third = "\amy string3";
+            Console.WriteLine($"{first}, {second}, {third}");
+        }
+    }
+}
